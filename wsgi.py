eb8a033f7a92283e51d38923a73caeec8850f60d
lebsral <larsbell@gmail.com>
2013-03-02 23:58:27 -0900

    changed mysite in wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 # Ken added this, only thing that is different from the example template (not counting settings file name)
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'trainings')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__),'mysite')))
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'trainings.settings'
 import django.core.handlers.wsgi
